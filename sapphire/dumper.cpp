c76e3b31f851b61bdb0990710ef6bb155fc14773
101123 <jasamarast@gmail.com>
2025-08-10 16:04:42 -0700

    Updated for latest patch, added plugin to git

--- sapphire/dumper.cpp
+++ sapphire/dumper.cpp
@@ -442,7 +442,7 @@ bool is_exception_hook( CONTEXT* context, uint64_t search, uint64_t replace, uin
 	return match;
 }
 
-#define START_WRITE_METHOD_RVA 0xCF08E28
+#define START_WRITE_METHOD_RVA 0xCE434F8
 #define CORRUPT_VALUE 0xDEADBEEFCAFEBEEF
 
 uint64_t dumper::start_write_value = 0;
