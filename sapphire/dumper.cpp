f91c3a296c98d42d394f297d7e2a41db4809029f
101123 <jasamarast@gmail.com>
2025-08-23 05:40:30 -0700

    Updated for latest patch

--- sapphire/dumper.cpp
+++ sapphire/dumper.cpp
@@ -442,7 +442,7 @@ bool is_exception_hook( CONTEXT* context, uint64_t search, uint64_t replace, uin
 	return match;
 }
 
-#define START_WRITE_METHOD_RVA 0xCF9A3A0
+#define START_WRITE_METHOD_RVA 0xCF331F8
 #define CORRUPT_VALUE 0xDEADBEEFCAFEBEEF
 
 uint64_t dumper::start_write_value = 0;
